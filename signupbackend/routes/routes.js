df088550a1b65790cb754e33e4528e87d0db4632
“[Navodhya <navodhya2011@gmail.com>
2022-07-05 15:52:21 +0530

    routes.js implementation

--- signupbackend/routes/routes.js
+++ signupbackend/routes/routes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const signUpTemplateCopy = require('../models/SignUpModels');
 
+
 router.post ('/signup', (request, response) => {
+
+
     const signedUpUser = new signUpTemplateCopy({
         id:request.body.id,
         firstName:request.body.firstName,
